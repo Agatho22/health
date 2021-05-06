b9ca99016d5af82f0e846e29827345c5a1c37e15
Agatho22 <never5373@naver.com>
2021-05-07 05:55:47 +0900

    21.05.07 만보기 클래스 수정

--- app/src/main/java/com/aman/health/StepActivity.java
+++ app/src/main/java/com/aman/health/StepActivity.java
@@ -1,4 +1,57 @@
 package com.aman.health;
 
-public class StepActivity {
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StepActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor stepCountSensor;
+    TextView tvStepCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step);
+
+        tvStepCount = (TextView)findViewById(R.id.tvStepCount);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(stepCountSensor == null) {
+            Toast.makeText(this, "걸음 감지 센서가 없습니다", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, stepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            tvStepCount.setText("Step Count : " + String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
