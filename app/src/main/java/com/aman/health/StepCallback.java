0377c4d8ad7e8a6287165c5b6a3bcff65d0ba63e
Agatho22 <never5373@naver.com>
2021-05-11 00:47:23 +0900

    21.05.11 만보기 수정

--- app/src/main/java/com/aman/health/StepCallback.java
+++
@@ -1,6 +0,0 @@
-package com.aman.health;
-
-public interface StepCallback {
-    void onStepCallback(int step); //서비스에서 스텝 변화 시 액티비티로 전달하는 콜백 함수
-    void onUnbindService(); // 서비스 언바인드 시 액티비티로 전달하는 콜백 함수
-}
