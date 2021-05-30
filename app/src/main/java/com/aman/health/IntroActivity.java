e9ad32020aa1805bf9806987c387188ba2f77090
82109 <hunie5174@gmail.com>
2021-05-30 23:40:43 +0900

    // 인트로 화면 적용 // 2021-05-30 23:40

--- app/src/main/java/com/aman/health/IntroActivity.java
+++ app/src/main/java/com/aman/health/IntroActivity.java
@@ -1,10 +1,11 @@
 package com.aman.health;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Bundle;
+
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -13,21 +14,17 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        //타이틀바 숨김
-        //getSupportActionBar().hide();
-
-        //2초 후 인트로화면 제거
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(IntroActivity.this, IntroActivity.class);
-                startActivity(intent);  //다음화면으로 넘어가기
-
-                //뒤로가기버튼을 통하여 Intro Screen을 오지못하도록 종료
-                finish();
-            }
-
-        }, 2000);
+        IntroThread introThread = new IntroThread(handler);
+        introThread.start();
     }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/aman/health/IntroThread.java
@@ -0,0 +1,25 @@
+package com.aman.health;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class IntroThread extends Thread {
+
+    private Handler handler;
+
+    public IntroThread(Handler handler) {
+        this.handler = handler;
+    }
+    @Override
+    public void run() {
+        super.run();
+        Message msg = new Message();
+        try {
+            Thread.sleep(1000);
+            msg.what = 1;
+            handler.sendEmptyMessage(msg.what);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
