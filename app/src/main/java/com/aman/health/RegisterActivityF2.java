a6415892421d5c57ec739309b93c20b781e882d1
Agatho22 <never5373@naver.com>
2021-09-15 21:13:05 +0900

    21:12 회원가입창 UI 수정(미완)

---
+++ app/src/main/java/com/aman/health/RegisterActivityF2.java
@@ -0,0 +1,186 @@
+package com.aman.health;
+
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class RegisterActivityF2 extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivityF";
+    private FirebaseAuth mfirebaseAuth; //파이어베이스 인증
+    private DatabaseReference mDatabaseRef; //실시간 데이터베이스
+    private EditText mEtEmail, mEtpwd, mEtpwd2, mEtName, mEtAge, mEtheight, mEtweight; //회원가입 입력필드
+    private RadioGroup rg_gender;
+    private String gender;
+    private Button mBtnRegister;
+    int age= 0;
+
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_f);
+
+
+        //파이어베이스 접근 설정
+        mfirebaseAuth = mfirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Users");
+        mEtAge = findViewById(R.id.et_age);
+        mEtAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(RegisterActivityF2.this, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+
+
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtpwd = findViewById(R.id.et_pwd);
+        mEtpwd2 = findViewById(R.id.et_pwd2);
+        mEtName = findViewById(R.id.et_name);
+        mEtheight = findViewById(R.id.et_height);
+        mEtweight = findViewById(R.id.et_weight);
+        rg_gender = findViewById(R.id.rg_gender);
+        mBtnRegister = findViewById(R.id.btn_registerf);
+
+
+        rg_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                RadioButton genderButton = (RadioButton)findViewById(i);
+                gender = genderButton.getText().toString();
+            }
+        });
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //회원가입 처리 시작
+                String strEmail = mEtEmail.getText().toString().trim();
+                String strPwd = mEtpwd.getText().toString().trim();
+                String strPwd2 = mEtpwd2.getText().toString().trim();
+
+                if(strPwd.equals(strPwd2)) {
+                    Log.d(TAG, "등록 버튼 " + strEmail + " , " + strPwd);
+                    final ProgressDialog mDialog = new ProgressDialog(RegisterActivityF2.this);
+                    mDialog.setMessage("가입중입니다...");
+                    mDialog.show();
+
+                    //파이어베이스에 신규계정 등록하기
+                    mfirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivityF2.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            //가입 성공시
+                            mDialog.dismiss();
+                            if (task.isSuccessful()) {
+
+                                Calendar cal = Calendar.getInstance();
+
+                                FirebaseUser user = mfirebaseAuth.getCurrentUser();
+                                String email = user.getEmail();
+                                String uid = user.getUid();
+                                String name = mEtName.getText().toString().trim();
+                                int age =cal.get (cal.YEAR)-myCalendar.get(Calendar.YEAR)+1;
+                                double height = Double.parseDouble(mEtheight.getText().toString());
+                                double weight = Double.parseDouble(mEtweight.getText().toString());
+
+                                //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                                HashMap<Object,String> hashMap = new HashMap<>();
+
+                                hashMap.put("Uid",uid);
+                                hashMap.put("Email",email);
+                                hashMap.put("Name",name);
+                                hashMap.put("Age",age+"");
+                                hashMap.put("Height",height+"");
+                                hashMap.put("Weight",weight+"");
+                                hashMap.put("Gender",gender);
+
+                                mDatabaseRef.child(uid).setValue(hashMap);
+
+
+                                //가입이 이루어져을시 가입 화면을 빠져나감.
+                                Intent intent = new Intent(RegisterActivityF2.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(RegisterActivityF2.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(RegisterActivityF2.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                                return;  //해당 메소드 진행을 멈추고 빠져나감.
+
+                            }
+
+                        }
+                    });
+
+                    //비밀번호 오류시
+                }else{
+
+                    Toast.makeText(RegisterActivityF2.this, "비밀번호가 틀렸습니다. 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
+    }
+
+    public boolean onSupportNavigateUp(){
+        onBackPressed();; // 뒤로가기 버튼이 눌렸을시
+        return super.onSupportNavigateUp(); // 뒤로가기 버튼
+    }
+
+    private void updateLabel() {
+        String myFormat = "yyyy/MM/dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+
+        EditText mEtAge = (EditText) findViewById(R.id.et_age);
+        mEtAge.setText(sdf.format(myCalendar.getTime()));
+
+    }
+
+}
