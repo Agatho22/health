61f1cc5de8d34503be6bbf49788e03d69dc937f3
MitZ4 <ydk0227@gmail.com>
2021-09-17 14:03:32 +0900

    ㅁㄴㅇㄹ

--- app/src/main/java/com/aman/health/ProFileFragment.java
+++ app/src/main/java/com/aman/health/ProFileFragment.java
@@ -1,9 +1,11 @@
 package com.aman.health;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,15 @@ public class ProFileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        Button gomyexercise = (Button)view.findViewById(R.id.profile);
+        gomyexercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (
+                        getContext().getApplicationContext(), Fbtest.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
